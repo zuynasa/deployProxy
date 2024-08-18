192010b86c879752f62383027f688bc8e7526e0c
zuynasa tru <wavermaya67@gmail.com>
2024-08-18 15:36:47 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0x140F76Cc70E9781c44c5A3CBc7cD87d6671E07F7'
+export const ProxyAdmin = '0x2D9a997fCcDBB92B2725716b8b9307CE94B5707c'
+export const SWTRImplementation = '0xbaCCb10899B25dcf2fb9E289a374acD9c7c5db96'
     
